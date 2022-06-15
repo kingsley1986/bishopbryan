b9e3dc31b57764fb6eeff253fd1d229b730083a5
kingsley <chukwumakingsley1@gmail.com>
2022-06-15 12:28:43 +0200

    about bishop and boards link

--- js/main.js
+++ js/main.js
@@ -132,6 +132,23 @@
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+
+
+
+    $(document).ready(function () {
+    $('.nav-toggle').click(function () {
+        var collapse_content_selector = $(this).attr('href');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('Read More');
+            } else {
+                toggle_switch.html('Read Less');
+            }
+        });
+    });
+
+});
     
 })(jQuery);
 
